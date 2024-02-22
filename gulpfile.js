57cd259be86a4f11c4f5a6d3447890337b496f0c
Daniel Escobar <daniescomoli@gmail.com>
2024-02-22 10:36:47 +0100

    intento de gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,47 @@
-const sass = require("gulp-sass");
-const sassdoc = require("sassdoc");
-const { src, dest, series } = require("gulp");
-const cssnano = require("cssnano");
-const rename = require("gulp-rename");
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('cssnano');
+const rename = require('gulp-rename');
+const sassdoc = require('sassdoc');
 
 function compilar() {
-    return src("scss/styles.scss")
+    return gulp.src("scss/styles.scss")
         .pipe(sass())
-        .pipe(dest("up/css/"));
+        .pipe(gulp.dest("up/css/"));
 }
 
 function minify_nano(){
-    return src("scss/styles.scss")
+    return gulp.src("scss/styles.scss")
     .pipe(sass())
     .pipe(cssnano())
     .pipe(rename({suffix:".nano", extname:".css"}))
-    .pipe(dest("up/css/css"));
+    .pipe(gulp.dest("up/css/"));
 }
 
 function todo(){
-    return src("scss/styles.scss")
+    return gulp.src("scss/styles.scss")
     .pipe(sass())
     .pipe(minify_nano())
     .pipe(rename({
         suffix:".min",
         extname:".css"
     }))
-    .pipe(dest("subir/assets/css/"));
+    .pipe(gulp.dest("subir/assets/css/"));
 }
 
 function generar_docs() {
-    return src("scss/styles.scss")
+    return gulp.src("scss/styles.scss")
         .pipe(sassdoc());
 }
 
 function html(){
-    return src(".html")
+    return gulp.src(".html")
     .pipe(processhtml())
-    .pipe("app")
+    .pipe(gulp.dest("app"));
 }
 
 exports.compilar = compilar;
 exports.generar_docs = generar_docs;
 exports.todo = todo;
 exports.html = html;
-exports.default = series(compilar, generar_docs, todo, html);
+exports.default = gulp.series(compilar, generar_docs, todo, html);
